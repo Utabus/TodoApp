36ccb0f1323bf9ae26a86b0abdefccf63fe0b20f
khangpm20a <0306201451@caothang.edu.vn>
2024-03-12 22:38:23 +0700

    login register jwt

--- PaymentAPI/PaymentAPI/obj/Debug/net6.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/PaymentAPI/obj/Debug/net6.0/PaymentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd54d928e890348995b9006a4ab69e0587c0fb45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fae577befdeadfb54108ac4dda3b46858bf7a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
